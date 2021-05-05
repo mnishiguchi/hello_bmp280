54a39163e7ceaa9b4eab71d59685908583ec83ec
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-05-05 16:42:45 -0400

    Initial

---
+++ lib/hello_bmp280.ex
@@ -0,0 +1,18 @@
+defmodule HelloBmp280 do
+  @moduledoc """
+  Documentation for HelloBmp280.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloBmp280.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hello_bmp280_test.exs
@@ -0,0 +1,8 @@
+defmodule HelloBmp280Test do
+  use ExUnit.Case
+  doctest HelloBmp280
+
+  test "greets the world" do
+    assert HelloBmp280.hello() == :world
+  end
+end
